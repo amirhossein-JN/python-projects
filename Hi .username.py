a7c4fd5c755de4a2b8d46aed79aa354646536e13
amirhossein-JN <amir.hossein.ebrahimi.jz@gmail.com>
2026-06-29 22:09:30 +0330

    ADDED Hi .username.py

---
+++ Hi .username.py
@@ -0,0 +1,18 @@
+username = input("Enter your username: ")
+
+print("Hello " + username)
+
+age = input("Enter your age: ")
+
+print("Your age is " + age)
+
+import random
+
+num = random.randint(0, 1)
+
+if num > 0.5:
+    print("shir")
+else:
+    print("khat")
+
+    
