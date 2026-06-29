6ac3e2b20a42acd47fbcd040f2aabbfcfc4add08
amirhossein-JN <amir.hossein.ebrahimi.jz@gmail.com>
2026-06-29 22:09:30 +0330

    ADDED p.txt.py

---
+++ p.txt.py
@@ -0,0 +1,16 @@
+a = int(input("enter the number"))
+
+b = int(input("enter the next number"))
+
+c =input("enter the operation")
+
+if c == "+":
+    print(a+b)
+elif c == "-":
+    print(a-b)
+elif c == "*":
+    print(a*b)
+elif c == "/":
+    print(a/b)
+else:
+    print("error")
